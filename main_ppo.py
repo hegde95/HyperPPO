905609872020e16dbee81ae5653e0ea1eda42c8e
hegde95 <hegdeshashank@yahoo.com>
2023-01-19 16:45:54 -0800

    increased number if minibatches

--- main_ppo.py
+++ main_ppo.py
@@ -58,7 +58,7 @@ def parse_args():
         help="the discount factor gamma")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=32,
+    parser.add_argument("--num-minibatches", type=int, default=125,
         help="the number of mini-batches")
     parser.add_argument("--update-epochs", type=int, default=10,
         help="the K epochs to update the policy")
